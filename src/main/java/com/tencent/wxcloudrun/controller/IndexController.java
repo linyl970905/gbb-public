67cb1db85b136b349f696dc2be6ff026a29dff8f
zero <1486413925@qq.com>
2022-10-26 15:30:00 +0800

    gbb-welcome-START

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -1,17 +1,19 @@
 package com.tencent.wxcloudrun.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * index控制器
  */
-@Controller
-
+@RestController
+@RequestMapping("/gbb")
 public class IndexController {
 
   /**
    * 主页页面
+   *
    * @return API response html
    */
   @GetMapping
@@ -19,4 +21,8 @@ public class IndexController {
     return "index";
   }
 
+  @GetMapping("/welcome")
+  public String welcomePage() {
+    return "Welcome To ... GBB!";
+  }
 }
