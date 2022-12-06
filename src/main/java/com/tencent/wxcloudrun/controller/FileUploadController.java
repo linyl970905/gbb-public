4fba27b355faa177bf84ffe3e40c11c474671866
zero <1486413925@qq.com>
2022-12-06 15:12:41 +0800

    1206Update

--- src/main/java/com/tencent/wxcloudrun/controller/FileUploadController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/FileUploadController.java
@@ -5,6 +5,8 @@ import cn.hutool.http.HttpException;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.json.JSONUtil;
+import com.tencent.wxcloudrun.config.ApiResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,17 +18,17 @@ import java.util.Map;
  * @Motto: Nothing is impossible
  */
 @RestController
-@RequestMapping("/file/upload")
+@RequestMapping("/upload/file")
 public class FileUploadController {
 
-    public static void main(String[] args) {
-
+    @GetMapping("/uploadFileParam")
+    public ApiResponse uploadFileParam() {
+        // 请求参数
         Map<String, Object> requestBody = MapUtil.newHashMap();
         requestBody.put("env", "prod-9gdfw13rcabb4e9a");
-        requestBody.put("path", "business/1.zip");
+        requestBody.put("path", "business/test.png");
 
-
-        HttpRequest httpRequest = HttpRequest.post("https://api.weixin.qq.com/tcb/uploadfile?cloudbase_access_token=ACCESS_TOKEN");
+        HttpRequest httpRequest = HttpRequest.post("https://api.weixin.qq.com/tcb/uploadfile");
         httpRequest.contentType("application/json");
         httpRequest.body(JSONUtil.toJsonStr(requestBody));
         httpRequest.setConnectionTimeout(18000);
@@ -35,22 +37,17 @@ public class FileUploadController {
 
         try {
             execute = httpRequest.execute();
-
-
         } catch (HttpException e) {
             e.printStackTrace();
         }
-
         if (execute == null) {
-            System.out.println("响应数据为空！");
+            return ApiResponse.error("提示：请求失败！");
         } else {
             if (execute.isOk()) {
                 String respBody = execute.body();
-
-                System.out.println(requestBody);
+                return ApiResponse.ok("提示：返回数据--" + respBody);
             }
         }
-
-        }
-
+        return ApiResponse.ok();
+    }
 }
